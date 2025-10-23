d77d7ceeb802a25ce6cdc73209eaba70ec841160
arrshsh <arsh311202@gmail.com>
2025-10-23 10:49:26 -0400

    Project structure set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Arsh Vahora",
+    author_email="vahora4@uwindsor.ca",
+    packages=find_packages()
+)
